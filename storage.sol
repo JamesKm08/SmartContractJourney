c11efde3a78bef7c286b943bf758a8233e927434
JamesKm08 <66277467+JamesKm08@users.noreply.github.com>
2023-05-23 06:38:22 +0300

    Update storage.sol

--- storage.sol
+++ storage.sol
@@ -1,10 +1,12 @@
 pragma solidity >=0.8.0 < 0.9.0;
 
 contract FirstStorage{
-  unit256 public myNo;
+  uint256 public myNo;
   
   function store(uint256 NewNo)public{
     
-    myNo = NewNo
-    }
+    myNo = NewNo;
+
+  }
 }
+
