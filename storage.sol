6935ed5031f40dcccb5e9048a0d77dcfca6295aa
JamesKm08 <66277467+JamesKm08@users.noreply.github.com>
2023-05-25 06:37:13 +0300

    Update storage.sol

--- storage.sol
+++ storage.sol
@@ -1,12 +1,21 @@
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 < 0.9.0;
 
 contract FirstStorage{
   uint256 public myNo;
   
+ //storing a number and be able to alter it
   function store(uint256 NewNo)public{
     
     myNo = NewNo;
 
   }
+  //viewing the stored number without altering it
+  function Retrieve() public view returns(uint256){
+
+      return(myNo);
+
+  }
 }
+
 
