dffd118a92984fd2ea8a88e394b15775913b9aa1
JamesKm08 <66277467+JamesKm08@users.noreply.github.com>
2023-05-25 06:40:14 +0300

    Update storage.sol

--- storage.sol
+++ storage.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.8.0 < 0.9.0;
 
 contract FirstStorage{
-  uint256 public myNo;
+  uint256 myNo;
   
  //storing a number and be able to alter it
   function store(uint256 NewNo)public{
