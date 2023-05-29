c203e3630b3def70b5f7cc3689d389c443561bed
JamesKm08 <66277467+JamesKm08@users.noreply.github.com>
2023-05-29 06:06:06 +0300

    Update storage.sol

--- storage.sol
+++ storage.sol
@@ -4,6 +4,9 @@ pragma solidity >=0.8.0 < 0.9.0;
 contract FirstStorage{
   uint256 myNo;
   
+ //The below line of code is storing a string with a number
+ People public person = People({myNo : 5, name: "Jim"});
+  
  //storing a number and be able to alter it
   function store(uint256 NewNo)public{
     
