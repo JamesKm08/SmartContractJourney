2e8f0910c51de6377155ab7feffacda5a95db10b
JamesKm08 <66277467+JamesKm08@users.noreply.github.com>
2023-06-09 06:40:58 +0300

    Update storage.sol

--- storage.sol
+++ storage.sol
@@ -1,24 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 < 0.9.0;
 
-contract FirstStorage{
-  uint256 myNo;
-  
- //The below line of code is storing a string with a number
- People public person = People({myNo : 5, name: "Jim"});
-  
- //storing a number and be able to alter it
-  function store(uint256 NewNo)public{
-    
-    myNo = NewNo;
+//importing from a file in same folder
+import "add the path of the file you want to import"
 
-  }
-  //viewing the stored number without altering it
-  function Retrieve() public view returns(uint256){
-
-      return(myNo);
-
-  }
+contract StorageFactory{
+ 
 }
 
 
