694123b4d986e2cd940e1bb0bc37d55726b136f3
JamesKm08 <66277467+JamesKm08@users.noreply.github.com>
2023-06-09 06:59:07 +0300

    Update storage.sol

--- storage.sol
+++ storage.sol
@@ -4,6 +4,9 @@ pragma solidity >=0.8.0 < 0.9.0;
 //importing from a file in same folder
 import "add the path of the file you want to import"
 
+//inheritance in solidity use the key work is eg.
+//contract StorageFactory is FirstStorage{}
+
 contract StorageFactory{
  
 }
